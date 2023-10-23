40f1aaa8ef0499ddf644dea31672b62174d41428
joselpc84 <joselpc84@gmail.com>
2023-10-23 15:23:14 -0400

    hacks passed del 1 al 10

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,10 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    list_res = list(result.upper())
+    list_res[1] = '0'
+    list_res[2] = '0'
+    list_res[4] = '1'
+    list_res[6] = '@'
+    result = list_res
     return result  
--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,8 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    #...
+    r = 6
+    while (r > 0):
+        result.append(r-1)
+        r-=1
     return result  
